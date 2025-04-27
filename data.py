e9860840aedaabb910aaf2a4e1f8824e8c2a2272
laura062 <lauleti798@gmail.com>
2025-04-26 20:01:06 -0600

    progreso proyecto8

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
-urban_routes_url = 'https://cnt-624bfd97-4b98-4da1-a29a-d5aecdb6b5ae.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-9f3c630d-2336-43a4-bc78-5ac9f8dbb739.containerhub.tripleten-services.com/?lng=es'
 address_from = "East 2nd Street, 601"
 address_to = "1300 1st St"
 phone_number = '+1 123 123 12 12'
-card_number, card_code = '1234 5678 9100', '111'
+card_number = '1234 5678 9100'
+card_code =  '111'
 message_for_driver = 'Muéstrame el camino al museo'
--- locators.py
+++
@@ -1,5 +0,0 @@
-from selenium.webdriver.common.by import By
-
-from_field = (By.ID,"from")
-to_field = (By.ID, "to")
-taxi_button = (By.XPATH, "//button[@type='button' and @class='button round']")
