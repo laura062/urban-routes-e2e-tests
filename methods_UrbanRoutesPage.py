c772e70ddd852fda29fb563428637d57a29d6328
laura062 <lauleti798@gmail.com>
2025-08-25 21:52:26 -0600

    proyecto8 de laura leticia

--- methods_UrbanRoutesPage.py
+++
@@ -1,7 +0,0 @@
-import time
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import locator
-from selenium.webdriver.common.by import By
-
